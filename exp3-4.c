aa1ef6fd7384b7c578129685b6e99a3823e9f649
Ziwen Zhao <844766049@qq.com>
2019-04-25 20:45:50 +0800

    Add some applications.

---
+++ exp3-4.c
@@ -0,0 +1,376 @@
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "hw_memmap.h"
+#include "debug.h"
+#include "gpio.h"
+#include "hw_i2c.h"
+#include "hw_types.h"
+#include "i2c.h"
+#include "pin_map.h"
+#include "sysctl.h"
+#include "systick.h"
+#include "interrupt.h"
+#include "uart.h"
+#include "string.h"
+
+#define SYSTICK_FREQUENCY		1000			//1000hz
+
+#define	I2C_FLASHTIME				500				//500mS
+#define GPIO_FLASHTIME			300				//300mS
+//*****************************************************************************
+//
+//I2C GPIO chip address and resigster define
+//
+//*****************************************************************************
+#define TCA6424_I2CADDR 					0x22
+#define PCA9557_I2CADDR						0x18
+
+#define PCA9557_INPUT							0x00
+#define	PCA9557_OUTPUT						0x01
+#define PCA9557_POLINVERT					0x02
+#define PCA9557_CONFIG						0x03
+
+#define TCA6424_CONFIG_PORT0			0x0c
+#define TCA6424_CONFIG_PORT1			0x0d
+#define TCA6424_CONFIG_PORT2			0x0e
+
+#define TCA6424_INPUT_PORT0				0x00
+#define TCA6424_INPUT_PORT1				0x01
+#define TCA6424_INPUT_PORT2				0x02
+
+#define TCA6424_OUTPUT_PORT0			0x04
+#define TCA6424_OUTPUT_PORT1			0x05
+#define TCA6424_OUTPUT_PORT2			0x06
+
+
+
+
+void 		Delay(uint32_t value);
+void 		S800_GPIO_Init(void);
+uint8_t 	I2C0_WriteByte(uint8_t DevAddr, uint8_t RegAddr, uint8_t WriteData);
+uint8_t 	I2C0_ReadByte(uint8_t DevAddr, uint8_t RegAddr);
+void		S800_I2C0_Init(void);
+void 		S800_UART_Init(void);
+void UARTStringPut( char *PutMessage);
+void UARTStringGet(const char signature);
+void clear(char *Stringget);
+void showtime(char *Stringput);
+//systick software counter define
+volatile uint16_t systick_10ms_couter,systick_100ms_couter;
+volatile uint8_t	systick_10ms_status,systick_100ms_status;
+
+volatile uint8_t result,cnt,key_value,gpio_status;
+volatile uint8_t rightshift = 0x01;
+uint32_t ui32SysClock;
+uint8_t seg7[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x58,0x5e,0x079,0x71,0x5c};
+char Stringget[30]={0};
+char Stringput[30]={0};
+uint8_t i=0, mon=0;
+uint8_t hour=23, minute=58, second=58,month;
+uint8_t xhour, xminute, xsecond, xmonth;
+
+int main(void)
+{
+	volatile uint16_t	i2c_flash_cnt,gpio_flash_cnt;
+	//use internal 16M oscillator, PIOSC
+   ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_16MHZ |SYSCTL_OSC_INT |SYSCTL_USE_OSC), 16000000);		
+	//ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_16MHZ |SYSCTL_OSC_INT |SYSCTL_USE_OSC), 8000000);		
+	//use external 25M oscillator, MOSC
+   //ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |SYSCTL_OSC_MAIN |SYSCTL_USE_OSC), 25000000);		
+
+	//use external 25M oscillator and PLL to 120M
+   //ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |SYSCTL_CFG_VCO_480), 120000000);;		
+	//ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_16MHZ |SYSCTL_OSC_INT | SYSCTL_USE_PLL |SYSCTL_CFG_VCO_480), 20000000);
+	
+  SysTickPeriodSet(ui32SysClock);
+	SysTickEnable();
+	SysTickIntEnable();
+   IntMasterEnable();		
+
+	
+	S800_GPIO_Init();
+	S800_I2C0_Init();
+	S800_UART_Init();
+	while (1)
+	{
+		UARTStringGet(' ');         //We will turn to the function to get the string
+		if (strncmp(Stringget,"SET",3)==0) 
+    {
+		/****************************time adjust part********************************/
+		xhour=(Stringget[3]-'0')*10+(Stringget[4]-'0');
+		xminute=(Stringget[6]-'0')*10+(Stringget[7]-'0');
+		xsecond=(Stringget[9]-'0')*10+(Stringget[10]-'0');
+		if(xsecond>=60){second = xsecond%60; xminute = xminute + xsecond/60;}
+		else {second = xsecond;}
+		if(xminute>=60){minute = xminute%60; xhour = xhour + xminute/60;}
+		else {minute = xminute;}
+    if(xhour>=24) hour = xhour%24;
+		else hour = xhour;
+ 		
+		showtime(Stringput);	
+		clear(Stringget);	
+		}
+		if (strncmp(Stringget,"INC",3)==0) 
+    {
+		/****************************time adjust part********************************/
+		xhour=(Stringget[3]-'0')*10+(Stringget[4]-'0')+hour;
+		xminute=(Stringget[6]-'0')*10+(Stringget[7]-'0')+minute;
+		xsecond=(Stringget[9]-'0')*10+(Stringget[10]-'0')+second;
+		if(xsecond>=60){second = xsecond%60; xminute = xminute + xsecond/60;}
+		else {second = xsecond;}
+		if(xminute>=60){minute = xminute%60; xhour = xhour + xminute/60;}
+		else {minute = xminute;}
+    if(xhour>=24) hour = xhour%24;
+		else hour = xhour;
+		
+		showtime(Stringput);			
+		clear(Stringget);	
+		}
+		if (strcmp(Stringget,"GETTIME")==0)
+		{
+		showtime(Stringput);	
+		clear (Stringget);
+		}
+		
+    /************************************month adjust part*******************************************/
+    if (strncmp(Stringget,"JAN",3)==0){month = 0; mon = 1;}
+		else if (strncmp(Stringget,"FEB",3)==0){month = 1; mon = 1;}
+		else if (strncmp(Stringget,"MAR",3)==0){month = 2; mon = 1;}
+		else if (strncmp(Stringget,"APR",3)==0){month = 3; mon = 1;}
+		else if (strncmp(Stringget,"MAY",3)==0){month = 4; mon = 1;}
+		else if (strncmp(Stringget,"JUN",3)==0){month = 5; mon = 1;}
+		else if (strncmp(Stringget,"JUL",3)==0){month = 6; mon = 1;}
+		else if (strncmp(Stringget,"AUG",3)==0){month = 7; mon = 1;}
+		else if (strncmp(Stringget,"SEB",3)==0){month = 8; mon = 1;}
+		else if (strncmp(Stringget,"OCT",3)==0){month = 9; mon = 1;}
+		else if (strncmp(Stringget,"NOV",3)==0){month = 10; mon = 1;}
+		else if (strncmp(Stringget,"DEC",3)==0){month = 11; mon = 1;}
+		
+		if (mon)
+		{
+		  xmonth = (Stringget[4]-'0')*10+(Stringget[5]-'0');
+			if(Stringget[3]-'+'==0){if(month + xmonth >=12)month=month+xmonth-12; else month = month + xmonth;}
+			if(Stringget[3]-'-'==0){if(month>=xmonth)month = month - xmonth; else month = month+(12-xmonth);}
+			if(month == 0) UARTStringPut("\r\nJAN\r\n");
+			else if(month == 1) UARTStringPut("\r\nFEB\r\n");
+			else if(month == 2) UARTStringPut("\r\nMAR\r\n");
+			else if(month == 3) UARTStringPut("\r\nAPR\r\n");
+			else if(month == 4) UARTStringPut("\r\nMAY\r\n");
+			else if(month == 5) UARTStringPut("\r\nJUN\r\n");
+			else if(month == 6) UARTStringPut("\r\nJUL\r\n");
+			else if(month == 7) UARTStringPut("\r\nAUG\r\n");
+			else if(month == 8) UARTStringPut("\r\nSEB\r\n");
+			else if(month == 9) UARTStringPut("\r\nOCT\r\n");
+			else if(month == 10) UARTStringPut("\r\nNOV\r\n");
+			else if(month == 11) UARTStringPut("\r\nDEC\r\n");
+			
+			clear (Stringget);
+			clear (Stringput);
+			mon = 0;
+		}
+		
+		
+		//seg show time!
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);                   //you need to clear the seg so that the shade can not be here
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[second%10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,128);
+		Delay(10000);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[second/10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,64);
+		Delay(10000);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,0x40);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,32);
+		Delay(10000);		
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[minute%10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,16);
+		Delay(10000);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[minute/10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,8);
+		Delay(10000);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,0x40);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,4);
+		Delay(10000);			
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[hour%10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0x02);
+		Delay(10000);	
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT1,seg7[hour/10]);
+		I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_OUTPUT_PORT2,0x01);
+		Delay(10000);
+
+		//second, minute and hour
+		if (second==60) {minute++; second=0;}
+		if (minute==60) {hour++; minute=0;}
+		if (hour>23) {hour=0;}
+	}
+
+
+
+}
+
+void Delay(uint32_t value)
+{
+	uint32_t ui32Loop;
+	for(ui32Loop = 0; ui32Loop < value; ui32Loop++){};
+}
+
+
+void UARTStringPut( char *PutMessage)
+{
+	while(*PutMessage!='\0')
+		UARTCharPut(UART0_BASE,*(PutMessage++));
+}
+
+void UARTStringGet(const char signature)
+{
+	while (UARTCharsAvail(UART0_BASE)) 
+	{
+		Stringget[i]=UARTCharGet(UART0_BASE);  //put in the number
+		if (Stringget[i]!=signature)
+		{
+			i++;
+		}
+		else 
+		{
+			Stringget[i]='\0';
+			i=0;
+			break;
+		}
+	}
+}
+
+void clear(char *Stringget)
+{
+	int k=0;
+	for (k=0; k<30; k++)
+	Stringget[k]=0;
+}
+
+void showtime(char *Stringput)
+{
+	Stringput[0]='T';Stringput[1]='I';Stringput[2]='M';Stringput[3]='E';
+	Stringput[4]=(hour/10)+'0';Stringput[5]=(hour%10)+'0';Stringput[6]=':';
+	Stringput[7]=(minute/10)+'0';Stringput[8]=(minute%10)+'0';Stringput[9]=':';
+	Stringput[10]=(second/10)+'0';Stringput[11]=(second%10)+'0';Stringput[12]='\0';
+	UARTStringPut(Stringput);
+	clear (Stringput);
+}
+
+
+
+
+
+
+void S800_UART_Init(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);						//Enable PortA
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA));			//Wait for the GPIO moduleA ready
+
+	GPIOPinConfigure(GPIO_PA0_U0RX);												// Set GPIO A0 and A1 as UART pins.
+  GPIOPinConfigure(GPIO_PA1_U0TX);    			
+
+  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+	// Configure the UART for 115,200, 8-N-1 operation.
+  UARTConfigSetExpClk(UART0_BASE, ui32SysClock,115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |UART_CONFIG_PAR_NONE));
+	UARTStringPut("\r\nHello, world!\r\n");
+}
+void S800_GPIO_Init(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);						//Enable PortF
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));			//Wait for the GPIO moduleF ready
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);						//Enable PortJ	
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ));			//Wait for the GPIO moduleJ ready	
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);						//Enable PortN	
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)){};			//Wait for the GPIO moduleN ready		
+	
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);			//Set PF0 as Output pin
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);			//Set PN0 as Output pin
+
+	GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE,GPIO_PIN_0 | GPIO_PIN_1);//Set the PJ0,PJ1 as input pin
+	GPIOPadConfigSet(GPIO_PORTJ_BASE,GPIO_PIN_0 | GPIO_PIN_1,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
+}
+
+void S800_I2C0_Init(void)
+{
+	uint8_t result;
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	GPIOPinConfigure(GPIO_PB2_I2C0SCL);
+  GPIOPinConfigure(GPIO_PB3_I2C0SDA);
+  GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+  GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+
+	I2CMasterInitExpClk(I2C0_BASE,ui32SysClock, true);										//config I2C0 400k
+	I2CMasterEnable(I2C0_BASE);	
+
+	result = I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_CONFIG_PORT0,0x0ff);		//config port 0 as input
+	result = I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_CONFIG_PORT1,0x0);			//config port 1 as output
+	result = I2C0_WriteByte(TCA6424_I2CADDR,TCA6424_CONFIG_PORT2,0x0);			//config port 2 as output 
+
+	result = I2C0_WriteByte(PCA9557_I2CADDR,PCA9557_CONFIG,0x00);					//config port as output
+	result = I2C0_WriteByte(PCA9557_I2CADDR,PCA9557_OUTPUT,0x0ff);				//turn off the LED1-8
+	
+}
+
+
+uint8_t I2C0_WriteByte(uint8_t DevAddr, uint8_t RegAddr, uint8_t WriteData)
+{
+	uint8_t rop;
+	while(I2CMasterBusy(I2C0_BASE)){};
+	I2CMasterSlaveAddrSet(I2C0_BASE, DevAddr, false);
+	I2CMasterDataPut(I2C0_BASE, RegAddr);
+	I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+	while(I2CMasterBusy(I2C0_BASE)){};
+	rop = (uint8_t)I2CMasterErr(I2C0_BASE);
+
+	I2CMasterDataPut(I2C0_BASE, WriteData);
+	I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+	while(I2CMasterBusy(I2C0_BASE)){};
+
+	rop = (uint8_t)I2CMasterErr(I2C0_BASE);
+	return rop;
+}
+
+uint8_t I2C0_ReadByte(uint8_t DevAddr, uint8_t RegAddr)
+{
+	uint8_t value,rop;
+	while(I2CMasterBusy(I2C0_BASE)){};	
+	I2CMasterSlaveAddrSet(I2C0_BASE, DevAddr, false);
+	I2CMasterDataPut(I2C0_BASE, RegAddr);
+	I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);		
+	I2CMasterControl(I2C0_BASE,I2C_MASTER_CMD_SINGLE_SEND);
+	while(I2CMasterBusBusy(I2C0_BASE));
+	rop = (uint8_t)I2CMasterErr(I2C0_BASE);
+	Delay(1);
+	//receive data
+	I2CMasterSlaveAddrSet(I2C0_BASE, DevAddr, true);
+//	I2CMasterControl(I2C0_BASE,I2C_MASTER_CMD_SINGLE_RECEIVE);
+	while(I2CMasterBusBusy(I2C0_BASE));
+	value=I2CMasterDataGet(I2C0_BASE);
+		Delay(1);
+	return value;
+}
+
+/*
+	Corresponding to the startup_TM4C129.s vector table systick interrupt program name
+*/
+void SysTick_Handler(void)
+{
+	second++;
+	if (GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0) == 0)
+	{
+		systick_100ms_status	= systick_10ms_status = 0;
+		GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0,GPIO_PIN_0);		
+	}
+	else
+		GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0,0);		
+}
